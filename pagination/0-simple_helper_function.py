2c77482fcadd8e07dc7ad6d616cb58a66756eafb
Davud <davudgeroqiq@gmail.com>
2026-06-13 12:37:45 +0000

    new commit

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
+"""Returns a tuple for pagination helper function"""
+
 
 def index_range(page, page_size):
+    """Returns a tuple for pagination helper function """
+
     return ((page - 1) * page_size, page * page_size)
